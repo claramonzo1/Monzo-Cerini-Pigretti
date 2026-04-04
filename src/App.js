e6939f75f868ec60cc14c41f41a5e6464f363b8b
renacerini <rrosmancerini@udesa.edu.ar>
2026-04-04 13:43:47 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -4,44 +4,38 @@ import Navbar from "./componentes/Navbar/Navbar";
 import Home from "./screens/Home/Home";
 import Detalle from "./screens/Detalle/Detalle";
 import Login from "./componentes/Login/Login";
-
-// import Register from "./componentes/Register/Register";
-// import Navbar from "./componentes/Navbar/Navbar";
-// import { Switch, Route } from "react-router-dom";
+import Register from "./componentes/Register/Register";
 
 function App() {
-  // let menu = [
-  //   {
-  //     nombre: "AboutUs",
-  //     Link: "/about"
-  //   },
-  //   {
-  //     nombre: "Contacto",
-  //     Link: "/contacto"
-  //   },
-  //   {
-  //     nombre: "Personajes",
-  //     Link: "/personajes"
-  //   }
-  // ];
+  let menu = [
+    {
+      nombre: "AboutUs",
+      Link: "/about"
+    },
+    {
+      nombre: "Contacto",
+      Link: "/contacto"
+    },
+    {
+      nombre: "Personajes",
+      Link: "/personajes"
+    }
+  ];
 
   return (
     <div className="App">
-      <Login />
-
-      {/*
       <Navbar elementos={menu} />
 
       <Switch>
         <Route path="/" exact={true} component={Home} />
-        <Route path="/about" component={AboutUs} />
+        <Route path="/login" component={Login} />
+        <Route path="/registro" component={Register} />
+        <Route path="/detalle/:tipo/:id" component={Detalle} />
       </Switch>
 
-      <Register />
       <footer>
         <p>Pigretti, Monzó y Cerini</p>
       </footer>
-      */}
     </div>
   );
 }
