d52f05d17f9c94901db26b98b9896590617c9149
Lucia Pigretti <luciapigretti@Lucias-MacBook-Air.local>
2026-04-05 17:58:47 -0300

    Hice serie.ja y arregle detalle.js

---
+++ src/componentes/Serie/Serie.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Serie.css";
+
+class Serie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      verDescripcion: false
+    };
+  }
+
+  toggleDescripcion() {
+    this.setState({
+      verDescripcion: this.state.verDescripcion === false ? true : false
+    });
+  }
+
+  render() {
+    return (
+      <article className="single-card-tv">
+        <img
+          src="{⁠https://image.tmdb.org/t/p/w500${this.props.datos.poster_path⁠}}"
+          className="card-img-top"
+          alt={this.props.datos.name}
+        />
+        <div className="cardBody">
+          <h5 className="card-title">{this.props.datos.name}</h5>
+
+          <button onClick={() => this.toggleDescripcion()}>
+            {this.state.verDescripcion === false ? "Ver descripción" : "Ocultar descripción"}
+          </button>
+
+          {this.state.verDescripcion === true ? (
+            <p className="card-text">{this.props.datos.overview}</p>
+          ) : null}
+
+          <link
+            className="btn btn-primary"
+            to="{⁠/detalle/tv/${this.props.datos.id}⁠}"
+          >
+            Ver más
+          </link>
+
+          <button className="btn alert-primary">🩶</button>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Serie;
